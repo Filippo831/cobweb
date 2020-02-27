358304059e57fc072b884f6e285dac7e94f996c5
Filippo Arduini <filippoarduini832@gmail.com>
2020-02-27 21:35:40 +0100

    inizion del cambiamento sfondo in base all'ora

--- javascript/wheather.js
+++ javascript/wheather.js
@@ -1,4 +1,5 @@
-let backgroundImage = document.querySelector(".navbar");
+let backgroundImage = document.querySelector(".navbar").style;
+let titleBackground = document.querySelector(".title").style;
 const key = "6370b6fcccf73f8ca1d58861c6f5915e";
 let longitudine;
 let latitudine;
@@ -14,15 +15,62 @@ if (navigator.geolocation) {
 } else {
     console.log("in not supported");
 }
+/*
 apiObject = JSON.parse(api);
 console.log(api);
 
+fetch(api).then();
 
-fetch(api)
-    .then()
+function readApi() {}
+*/
+//  cambia background in base all'ora
 
+// let hour = new Date().getHours();
+let hour = 10;
+let alba = 8;
+let tramonto = 18;
+let mid = 14;
+let red;
+let green;
+let blue;
+let firstColor;
+let secondColor;
 
+if (hour <= alba) {
+    red = 0;
+    green = 0;
+    blue = 100 + hour * 10;
+    console.log("meno alba");
+} else if (hour < mid) {
+    red = (hour - alba) * 20;
+    green = (hour - alba) * 30;
+    blue = 100 + (hour - alba) * 20;
 
-function readApi() {
+    console.log("meno mid");
+} else if (hour <= tramonto - 1) {
+    red = 50 + (hour - alba) * 12;
+    green = 250 - (hour - alba) * 10;
+    blue = 250 - (hour - alba) * 5;
+    console.log("meno tramonto-1");
+} else if (hour <= tramonto) {
+    red = 100 + (hour - alba) * 10;
+    green = 200 - (hour - alba) * 7;
+    blue = 200 - (hour - alba) * 5;
 
+    console.log("meno tramonto");
+} else if (hour > tramonto) {
+    red = (24 - hour) * 20;
+    green = (24 - hour) * 20;
+    blue = 100 + (24 - hour) * 10;
+
+    console.log("piu tramonto");
 }
+console.log(red, green, blue);
+console.log(hour);
+
+firstColor = `rgb(${red}, ${green},${blue})`;
+secondColor = `rgb(${red + 30}, ${green + 20},${blue - 20})`;
+let gradient = `linear-gradient(to right bottom, ${firstColor}, ${secondColor})`;
+
+titleBackground.background = gradient;
+backgroundImage.background = gradient;
