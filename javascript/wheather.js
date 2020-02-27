9470575fe4a77a7e4bd05edca094ab2c8843e088
Filippo831 <filippoarduini832@gmail.com>
2020-02-27 18:47:14 +0100

    chiedi locazione

--- javascript/wheather.js
+++ javascript/wheather.js
@@ -2,25 +2,13 @@ let backgroundImage = document.querySelector(".navbar");
 const key = "6370b6fcccf73f8ca1d58861c6f5915e";
 let longitudine;
 let latitudine;
-
-function getGeoLocation() {
+document.addEventListener("click", () => {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        navigator.geolocation.getCurrentPosition(position => {
+            console.log("HAI DATO I PERMESSI");
+            console.log(position);
+        });
     } else {
         console.log("in not supported");
     }
-}
-function onSuccess(position) {
-    alert("ciao");
-    console.log("HAI DATO I PERMESSI");
-    console.log(position);
-    const api = `api.openweathermap.org/data/2.5/weather?lat=${latitudine}&lon=${longitudine}&appid=${key}`;
-    console.log(api);
-}
-function onError() {
-    console.log("NON HAI I PERMESSI");
-}
-
-window.addEventListener("load", () => {
-    getGeoLocation();
 });
